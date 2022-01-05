ecbc0ebc5b0b5ee7d4103ee65c33c79718de2594
cwmat <chaz.mateer@gmail.com>
2022-01-04 19:29:13 -0500

    Add github link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,17 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import PropTypes from 'prop-types';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import React from "react";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
 
 const Header = (props) => {
   const barHeight = props?.height || '5vh';
+
+  const handleGithubClick = () => {
+    window.open('https://github.com/cwmat/geo-format');
+  }
 
   return (
     <AppBar
@@ -26,6 +33,26 @@ const Header = (props) => {
           }}
         >
           GeoFormat
+        </Typography>
+
+        <Typography
+          component={'span'}
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{
+            height: barHeight,
+            minHeight: barHeight,
+            position: 'absolute',
+            right: '2rem',
+            top: '0.25rem',
+          }}
+        >
+        <Tooltip title="Github Link">
+          <IconButton onClick={handleGithubClick}>
+            <GitHubIcon />
+          </IconButton>
+        </Tooltip>
         </Typography>
 
       </Toolbar>
